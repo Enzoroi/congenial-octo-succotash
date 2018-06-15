b785c138d5d19ca6ea32dddcb6c40da460706ae0
Enzoroi <40302058+Enzoroi@users.noreply.github.com>
2018-06-15 13:41:29 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    float Nota1;
+float Nota2;
+float Media;
+printf("\n Insira nota1:");
+scanf("%f",Nota1);
+printf("\n Insira nota2:");
+scanf("%f",Nota2);
+printf("Media das notas: \n %f",(Nota1+Nota2)/2);
+return 0;
+}
